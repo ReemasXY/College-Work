5203f86da111cbfdedddf44727eaa4c12bbd284b
ReemasXY <sameermhr345@gmail.com>
2025-02-06 20:52:01 +0545

    added new files

---
+++ BIM_3rd_sem/DSA/List/src/DeleteList.java
@@ -0,0 +1,120 @@
+
+
+
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class DeleteList {
+    static Node head =null;
+    
+    public static void main(String[] args) {
+        InsertAtFirst(100);
+        InsertAtFirst(200);
+        InsertAtFirst(300);
+        InsertAtFirst(400);
+        InsertAtFirst(500);
+        traverse();
+        deleteFromSpecificPosition(3);
+        DeleteFromFirst();
+        DeleteFromLast();
+        traverse();
+
+}
+      public static void InsertAtFirst(int data)
+    {
+       Node node = new Node(data,null);
+       if(head==null)
+       {
+           head =node;
+       }
+       else
+       {
+           node.next = head;
+           head = node;
+       }
+       
+       
+    }
+       //displaying all element of list
+    public static void traverse()
+    {
+        Node temp = head;
+        System.out.print("{");
+        while(temp!=null)
+        {
+            System.out.print(temp.data + "\t"); 
+            temp= temp.next;
+        }
+        
+    }
+    public static void DeleteFromFirst()
+    {
+        if(head==null)
+        {
+           System.out.println("List is empty"); 
+        }
+        else{
+            System.out.println("\n Data being deleted  from first is "+head.data);
+            head=head.next;
+    }
+}
+     public static void DeleteFromLast()
+    {
+        if(head==null)
+        {
+           System.out.println("List is empty"); 
+        }
+        else{
+     Node temp1=head;
+     Node temp2=head;
+     while(temp2.next!=null)
+     {
+         temp1=temp2;
+         temp2=temp2.next;
+     }
+     temp1.next=null;
+       System.out.println("\n Data being deleted  from last is "+temp2.data);
+     temp2=null;
+    }
+}
+       // Method to delete node from specific position
+    public static void deleteFromSpecificPosition(int pos) {
+        if (head == null) {
+            System.out.println("Nothing to delete");
+        } else {
+            Node temp1 = head;
+            int count = 1;
+            Node temp2;
+            while (temp1 != null) {
+                count++;
+                temp2 = temp1;
+                temp1 = temp1.next;
+                if (count == pos) {
+                    temp2.next = temp1.next;
+                    break;
+                }
+            }
+            System.out.println("\n Data being delted from "+ pos+ " position is " + temp1.data);
+        }
+}
+}
+class Node
+{
+    int data;
+    Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+    
+}
+
---
+++ BIM_3rd_sem/DSA/List/src/SinglyList.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import javax.swing.JOptionPane;
+public class SinglyList {
+    static Node head =null;
+    
+    public static void main(String[] args) {
+        InsertAtFirst(500);
+        InsertAtFirst(400);
+        InsertAtFirst(600);
+        InsertAtFirst(700);
+        InsertAtLast(800);         
+        InsertAtSpeciPos(500);
+        traverse();
+    }
+    
+    
+    public static void InsertAtFirst(int data)
+    {
+       Node node = new Node(data,null);
+       if(head==null)
+       {
+           head =node;
+       }
+       else
+       {
+           node.next = head;
+           head = node;
+       }
+       
+       
+    }
+    
+    
+    public static void InsertAtLast(int data)
+    {
+       Node node = new Node(data,null);
+       Node temp = head;
+       if(head==null)
+       {
+           head =node;
+       }
+       else
+       {
+           while(temp.next!=null)
+           {
+               temp = temp.next;
+           }
+           temp.next = node;
+          
+       }
+       
+       
+    }
+    
+    
+    
+      public static void InsertAtSpeciPos(int data)
+    {
+         Node node = new Node(data,null);
+       //Scanner sc = new Scanner(System.in);   
+       //System.out.println("Please Enter the position where you want to insert !!!");
+       int pos = Integer.parseInt(JOptionPane.showInputDialog("Please Enter the position where you want to insert !!!"));
+       
+       int size=0;
+       Node temp = head;
+       while(temp!=null)
+       {
+          temp = temp.next;
+          size++;
+       }
+       
+       if(pos > size)
+       {
+         JOptionPane.showMessageDialog(null, "Sorry !!! Unable to insert at your position");  
+       }
+       else
+       {
+         temp = head;
+         int i=1;
+         while(i<pos-1)
+         {
+             temp= temp.next;
+             i++;
+         }
+         
+         node.next= temp.next;
+         temp.next = node;
+            
+           
+       }
+       
+       
+       
+       
+    }
+    
+    
+    
+    
+    //displaying all element of list
+    public static void traverse()
+    {
+        Node temp = head;
+        System.out.print("{");
+        while(temp!=null)
+        {
+            System.out.print(temp.data + "\t"); 
+            temp= temp.next;
+        }
+        
+    }
+    
+}
+// Creation of node
+
+class Node
+{
+    int data;
+    Node next;
+
+    public Node(int data, Node next) {
+        this.data = data;
+        this.next = next;
+    }
+    
+}
+
+
